0dd6cde7f14aa525aa4f3275b5ba19399f909d78
Fabricio Catae <fcatae@hotmail.com>
2021-06-12 15:53:16 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/fcatae/go-generate-token
+
+go 1.16
+
+require (
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type Token struct {
+	AccessToken string `json:"access_token"`
+}
+
+func main() {
+	var tok Token
+
+	scopeDriveReadOnly := "https://www.googleapis.com/auth/drive.readonly"
+	endpoint := "https://oauth2.googleapis.com/token"
+	clientId := os.Args[1]
+	clientSecret := os.Args[2]
+	refreshToken := os.Args[3]
+
+	data := url.Values{}
+	data.Set("grant_type", "refresh_token")
+	data.Set("client_id", clientId)
+	data.Set("client_secret", clientSecret)
+	data.Set("refresh_token", refreshToken)
+	data.Set("scope", scopeDriveReadOnly)
+
+	body := strings.NewReader(data.Encode())
+
+	client := http.DefaultClient
+	req, _ := http.NewRequest("POST", endpoint, body)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Printf("ERROR Request=%s", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	json.NewDecoder(res.Body).Decode(&tok)
+
+	fmt.Print(tok.AccessToken)
+}
